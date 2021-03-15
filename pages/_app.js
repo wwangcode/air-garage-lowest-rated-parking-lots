6ff75fdd32ae97f26027c9f9551f4b0ead83fe17
willywangatx <62160201+willywangatx@users.noreply.github.com>
2021-03-15 17:40:58 -0500

    added head metadata - title

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <title>AirGarage - lowest rated parking lots</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
